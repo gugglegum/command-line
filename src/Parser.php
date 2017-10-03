da991b080bb21ea62622f05c17f2996109f6aae8
gugglegum <gugglegum@gmail.com>
2017-10-04 02:21:00 +0500

    Added optional parameter $startFromOffset

--- src/Parser.php
+++ src/Parser.php
@@ -8,10 +8,21 @@ use gugglegum\CommandLine\Options\OptionFlag;
 
 class Parser
 {
-    public static function parse(array $argv, Config $config)
+    /**
+     * Parses array of command line parameters (usually $_SERVER['argv']) according to command-line
+     * configuration. Parsed parameters are stored in the configuration and can be retrieved by
+     * `$config->getArgument('arg1')->getValue()` or `$config->getOption('opt1')->getValue()`.
+     *
+     * @param array  $argv              Array to parse (usually $_SERVER['argv'])
+     * @param Config $config            Configuration of command line (arguments, options)
+     * @param int    $startFromOffset   OPTIONAL Start parse arguments from this position (usually parameters in
+     *                                  $argv starts from 1, offset 0 contains path to executable)
+     * @throws Exception
+     */
+    public static function parse(array $argv, Config $config, $startFromOffset = 1)
     {
         $argumentIndex = 0;
-        for ($i = 1; $i < count($argv); $i++) {
+        for ($i = $startFromOffset; $i < count($argv); $i++) {
             if (strlen($argv[$i]) > 0 && $argv[$i]{0} === '-') { // if $argv[$i] is some option (maybe short, maybe long)
                 if (strlen($argv[$i]) > 1 && $argv[$i]{1} === '-') { // long option
                     if (($equalCharOffset = strpos($argv[$i], '=', 2)) !== false) {
